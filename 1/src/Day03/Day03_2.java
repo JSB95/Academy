191450b7233b5011c4a82460b645a7901a76a7e5
504 <504@DESKTOP-5A5LKLO>
2022-03-03 13:26:56 +0900

    20220303

--- 1/src/Day03/Day03_2.java
+++ 1/src/Day03/Day03_2.java
@@ -59,6 +59,51 @@ public class Day03_2 { // c s
 		if(max2 < int3_3) { max = int3_3; }
 		if(max2 < int3_4) { max = int3_4; }
 		System.out.println("가장 큰 수 : " + max2);
+		
+		// 문제 4 : 3개의 정수를 입력받아서 오름차순
+		System.out.println("정수1 입력");
+		int int4_1 = scanner.nextInt();
+		System.out.println("정수2 입력");
+		int int4_2 = scanner.nextInt();
+		System.out.println("정수3 입력");
+		int int4_3 = scanner.nextInt();
+		
+		if (int4_1 > int4_2) {			// > : 오름차순, < : 내림차순
+			// 교체 [스왑 = 두개 변수에 데이터 교환방법]
+			int temp = int4_1;			// 임시변수 = 변수1
+			int4_1 = int4_2;			// 변수1 = 변수2
+			int4_2 = temp;				// 변수2 = 임시변수
+		}
+		if (int4_1 > int4_3) { int temp = int4_1; int4_1 = int4_3; int4_3 = temp;
+		}
+		if (int4_2 > int4_3) { int temp = int4_2; int4_2 = int4_3; int4_3 = temp;
+		}
+		System.out.printf("입력한 값 오름차순 : %d %d %d", int4_1, int4_2, int4_3); 
+		
+		// 문제 5 : 4개의 정수를 입력받아서 내림차순
+		System.out.println("정수1 입력");
+		int int5_1 = scanner.nextInt();
+		System.out.println("정수2 입력");
+		int int5_2 = scanner.nextInt();
+		System.out.println("정수3 입력");
+		int int5_3 = scanner.nextInt();
+		System.out.println("정수4 입력");
+		int int5_4 = scanner.nextInt();
+		
+		if(int5_1 < int5_2) { int temp = int5_1; int5_1 = int5_2; int5_2 = temp;
+		}
+		if(int5_1 < int5_3) { int temp = int5_1; int5_1 = int5_3; int5_3 = temp;
+		}
+		if(int5_1 < int5_4) { int temp = int5_1; int5_1 = int5_4; int5_4 = temp;
+		}
+		if(int5_2 < int5_3) { int temp = int5_2; int5_2 = int5_3; int5_3 = temp;
+		}
+		if(int5_2 < int5_4) { int temp = int5_2; int5_2 = int5_4; int5_4 = temp;
+		}
+		if(int5_3 < int5_4) { int temp = int5_3; int5_3 = int5_4; int5_4 = temp;
+		}
+		System.out.printf("입력한 값 내림차순 : %d %d %d %d", int5_1, int5_2, int5_3, int5_4);
+		
 	} // m e
 
 } // c e
