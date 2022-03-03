f95779be886d9a7e282513eaf2ea9629c6151039
504 <504@DESKTOP-5A5LKLO>
2022-03-03 13:27:05 +0900

    20220303

--- 1/src/Day03/Day03_4.java
+++ 1/src/Day03/Day03_4.java
@@ -1,10 +1,13 @@
 package Day03;
 
-public class Day03_4 {
+public class Day03_4 { // c s
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) { // m s
+		
+		// 제어문 : 1. if 2. switch
+			// if : 논리검사(true or false) -> 경우의수가 정해져 있지 않은 경우
+			// switch : 데이터검사 -> 경우의수가 정해져 있는 경우
 
-	}
+	} // m e
 
-}
+} // c e
